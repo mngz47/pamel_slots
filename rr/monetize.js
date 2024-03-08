e7c6c7ece7af5d069568d7cb149b55a6183d0f45
Matthew <mngz636@gmail.com>
2024-03-08 07:46:33 -0800

    Update monetize.js

--- rr/monetize.js
+++ rr/monetize.js
@@ -11,12 +11,12 @@
    import { payfast } from "https://mngz47.github.io/F-Snake/rr/payfast.js";
 
 
-var credits;
+var credits = 0;
 
 
 function showCredits(){
 
-  document.body.innerHTML += "<h4 style='position:absolute;float:right;' >"+credits+"</h4>";
+  document.body.innerHTML += "<h4 style='position:absolute;float:right;' >CREDITS: "+credits+"</h4>";
 
 }
 //userId
