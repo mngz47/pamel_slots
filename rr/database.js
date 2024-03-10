253a3e9970a609aa295eb1e1bd55d443e2ad597d
Matthew <mngz636@gmail.com>
2024-03-10 07:00:46 -0700

    Update database.js

--- rr/database.js
+++ rr/database.js
@@ -1,3 +1,8 @@
+
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
+ 
 class Database{
 
  constructor() {
@@ -6,11 +11,7 @@ class Database{
  }
   const userId = 0;
 
-
-// Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
+ // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
