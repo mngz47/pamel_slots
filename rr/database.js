aa93e8acdf7013fcfbedbf0b1953bf0b6b93fa9f
Matthew <mngz636@gmail.com>
2024-03-10 07:02:36 -0700

    Update database.js

--- rr/database.js
+++ rr/database.js
@@ -1,10 +1,12 @@
 
+
+class Database{
+
 // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
  
-class Database{
-
+  
  constructor() {
 
    
